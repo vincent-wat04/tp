8eda6db18137b3da81c92756f73f33a5c09f0b75
Benn Tan <btjm123@gmail.com>
2025-10-09 10:46:46 +0800

    Remove redundant white spaces

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -66,8 +66,6 @@ public class AddCommand extends Command {
             }
         }
 
-
-
         model.addPerson(toAdd);
         return new CommandResult(String.format(MESSAGE_SUCCESS, Messages.format(toAdd)));
     }
