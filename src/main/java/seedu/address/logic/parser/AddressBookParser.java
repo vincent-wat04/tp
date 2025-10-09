f756c63c8113b9ee381066a50474f9dcfb24bc41
“Qu <vincent_wat@outlook.com>
2025-10-09 20:05:36 +0800

    Remove FilterCompanyCommand to reduce command count
    - Remove FilterCompanyCommand and related parser
    - Remove CompanyContainsPredicate
    - Remove all related tests
    - Unregister command from AddressBookParser
    
    Rationale:
    - Team prefers fewer commands
    - Company search will be integrated into find command in future PR
    - This PR focuses on adding Company field infrastructure only
    
    Core Company field functionality retained:
    - Company model class
    - Add/Edit command support for company (c/ prefix)
    - Storage and UI support
    - All tests pass (267 tests)

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -14,7 +14,6 @@ import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.ExitCommand;
-import seedu.address.logic.commands.FilterCompanyCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
@@ -77,9 +76,6 @@ public class AddressBookParser {
 
         case HelpCommand.COMMAND_WORD:
             return new HelpCommand();
-
-        case FilterCompanyCommand.COMMAND_WORD:
-            return new FilterCompanyCommandParser().parse(arguments);
 
         default:
             logger.finer("This user input caused a ParseException: " + userInput);
