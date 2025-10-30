7170f99111f16cc6cb79b655b4b4427986361dfe
“Qu <vincent_wat@outlook.com>
2025-10-30 19:07:52 +0800

    Fix inconsistency between allowed tag registry and sample utility data's tags

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -24,15 +24,15 @@ public class SampleDataUtil {
             new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
                 new Address("Blk 30 Geylang Street 29, #06-40"), new Company("Google"),
                 new NextMeeting("Project sync on 12 Oct, 2pm"),
-                getTagSet("friends")),
+                getTagSet("friend")),
             new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
                 new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), new Company("Microsoft"),
                 new NextMeeting("Client call on 15 Oct, 10am"),
-                getTagSet("colleagues", "friends")),
+                getTagSet("colleague", "friend")),
             new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
                 new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), new Company(""),
                 new NextMeeting(NextMeeting.DEFAULT_VALUE),
-                getTagSet("neighbours")),
+                getTagSet("classmate")),
             new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
                 new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), new Company("Apple"),
                 new NextMeeting("Design review on 18 Oct, 4pm"),
@@ -40,11 +40,11 @@ public class SampleDataUtil {
             new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
                 new Address("Blk 47 Tampines Street 20, #17-35"), new Company(""),
                 new NextMeeting(NextMeeting.DEFAULT_VALUE),
-                getTagSet("classmates")),
+                getTagSet("classmate")),
             new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
                 new Address("Blk 45 Aljunied Street 85, #11-31"), new Company("Google"),
                 new NextMeeting("Demo day on 20 Oct, 5pm"),
-                getTagSet("colleagues"))
+                getTagSet("colleague"))
         };
     }
 
